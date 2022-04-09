42edd374a9eaf018092eeb99f450f5f5a32db251
m.dogukansevinc <m.dogukansevinc@gmail.com>
2022-04-09 20:12:03 +0300

    Project created.

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:task_project/screens/upload_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Task Project',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: UploadScreen(),
+    );
+  }
+}
+
+
